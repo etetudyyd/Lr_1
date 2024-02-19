32635c5ace934231893fa8213433c6bd1fb076cb
Ryzhkov Dima <dima.ryzhkov@gmail.com>
2024-02-20 00:50:53 +0200

    Add class Hello

--- Lr_1/Program.cs
+++ Lr_1/Program.cs
@@ -2,4 +2,22 @@
 /// Коментарі для створення XML-документації.
 /// </summary>
 
-Console.WriteLine("Hello, World!");
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+
+        Hello hello = new Hello();
+
+        hello.SayHello();
+    }
+}
+
+class Hello
+{
+    public void SayHello()
+    {
+        Console.WriteLine("Hello!");
+    }
+}
