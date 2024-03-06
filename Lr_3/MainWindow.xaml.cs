489314cdc550a40423156efb47cfd6def990f439
Ryzhkov Dima <dima.ryzhkov@gmail.com>
2024-03-06 03:07:37 +0200

    Lr_3 Final Version

---
+++ Lr_3/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Data;
+using System.Windows;
+using System.Windows.Controls;
+
+
+namespace Lr_3
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            foreach (UIElement el in GroupButton.Children)
+            {
+
+                if (el is Button)
+                {
+                    ((Button)el).Click += Button_Click;
+                }
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string textButton = ((Button)e.OriginalSource).Content.ToString();
+
+                if (textButton == "C")
+                {
+                    text.Clear();
+                }
+                else if (textButton == "=")
+                {
+                    text.Text = new DataTable().Compute(text.Text, null).ToString();
+                }
+                else text.Text += textButton;
+
+            }
+            catch(Exception ex) 
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
